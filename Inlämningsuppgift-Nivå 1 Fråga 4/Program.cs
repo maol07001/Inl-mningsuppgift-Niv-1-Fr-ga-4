5709c6b78f385e741a846c623a062b85f9ceb028
mariu <mariu@DESKTOP-CT70FK7>
2024-12-16 16:28:01 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1 Fråga 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lösenord;
+            do
+            {
+                Console.Write("Ange lösenord:");
+                lösenord = Console.ReadLine(); 
+            }
+            while (lösenord != "hemligt"); 
+
+            Console.WriteLine("Lösenordet är rätt!");
+        }
+    }
+}
